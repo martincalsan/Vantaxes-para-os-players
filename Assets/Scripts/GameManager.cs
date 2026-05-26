c334a5d544f89859c2a999b11d1c6f6ec7195511
martincalsan <lordtheiax@gmail.com>
2026-05-26 08:05:22 +0200

    funcionamiento correcto

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 
@@ -8,9 +10,18 @@ public class GameManager : NetworkBehaviour
     readonly NetworkVariable<int> _mode = new(0);
     static readonly string[] ModeNames = { "Server Authority", "Server + Prediction", "Client Authority" };
 
+    const float MinInterval    = 5f;
+    const float MaxInterval    = 15f;
+    const float EffectDuration = 10f;
+
     public int Mode => _mode.Value;
 
     void Awake() => Instance = this;
+
+    public override void OnNetworkSpawn()
+    {
+        if (IsServer) StartCoroutine(EffectLoop());
+    }
 
     void OnGUI()
     {
@@ -29,4 +40,21 @@ public class GameManager : NetworkBehaviour
 
     [Rpc(SendTo.Server, InvokePermission = RpcInvokePermission.Everyone)]
     void RequestModeChangeServerRpc() => _mode.Value = (_mode.Value + 1) % 3;
+
+IEnumerator EffectLoop()
+{
+    while (true)
+    {
+        yield return new WaitForSeconds(Random.Range(MinInterval, MaxInterval));
+
+        var available = new List<PlayerMovement>();
+        foreach (var p in FindObjectsByType<PlayerMovement>(FindObjectsSortMode.None))
+            if (p.CurrentEffect == 0) available.Add(p);
+
+        if (available.Count == 0) continue;
+
+        var target = available[Random.Range(0, available.Count)];
+        target.ApplyEffect(Random.Range(0, 2) == 0 ? 1 : 2, EffectDuration);
+    }
+}
 }
